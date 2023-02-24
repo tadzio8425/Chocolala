021fbf22a3d9505a3ad700cabb57d1e36c4fa097
Correaln47 <correaln47@gmail.com>
2023-02-24 12:12:16 -0500

    Prueba Clase

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 //Librerias
 #include <Arduino.h>
-#include <FirebaseT.h>
+//#include <FirebaseT.h>
 #include <Balanza.h>
 #include <MotoBomba.h>
 #include <PID_v1.h>
@@ -11,6 +11,7 @@ double referencia_pid, entrada_pid, salida_pid;
 //Definición del objeto PID así como de las constantes
 
 double Kp=6.1, Ki=0.3, Kd=0.75; //
+double Kp_prueba;
 PID myPID(&entrada_pid, &salida_pid, &referencia_pid, Kp, Ki, Kd, DIRECT);
 
 //Variables - Pines Balanza
@@ -32,11 +33,12 @@ bool toggle = false;
 
 bool first_time = true;
 int time_mark;
+bool volumen_alcanzado = false;
 
 //Instanciación de objetos
 Balanza balanza(DOUT, CLK);
 MotoBomba motoBomba(pump_PWM);
-FirebaseT firebaseHandler;
+//FirebaseT firebaseHandler;
 
 //Aux Vars
 int timer_start;
@@ -48,11 +50,11 @@ void setup() {
   Serial.begin(115200);
 
   //Modo wireless (WiFi + Firebase)
-  if(wireless_mode){
+  /*if(wireless_mode){
     firebaseHandler.setWiFi("Chocolala", "eusebio8425");
     firebaseHandler.setFirebase("AIzaSyAiaVAvazH57Fce9ZsE9Cm06BxBMsoJXXw", "https://chocolala-e8384-default-rtdb.firebaseio.com/");
   }
-
+*/
   //Configuración de la motobomba y pwm
   motoBomba.setUp();
   motoBomba.set_speed(0);
@@ -62,32 +64,20 @@ void setup() {
 
 
   //Inicialización del controlador PID
-  myPID.SetOutputLimits(85, 255);
+  myPID.SetOutputLimits(45, 255);
   entrada_pid = balanza.get_volumen(1, 10);
-  referencia_pid = 200; //Valor de volumen (mL) a alcanzar en estado estable
+  referencia_pid = 400; //Valor de volumen (mL) a alcanzar en estado estable
 
-  if(referencia_pid <= 50){
-    myPID.SetTunings(16,3,0.5);
-    myPID.SetOutputLimits(180, 255);
+  if(referencia_pid<250){
+    Kp_prueba=35;
+  } 
+  else if(referencia_pid<300){
+    Kp_prueba=18;
   }
-  else if(referencia_pid <= 100){
-    myPID.SetTunings(6.1,0.32,0.75);
+  else{
+    Kp_prueba=6.2;
   }
-  else if(referencia_pid <= 200){
-    myPID.SetTunings(6.2,0.1,2.69);
-  }
-  else if(referencia_pid <= 300){
-    myPID.SetTunings(6.2,0.1,3.82);
-  }
-  else if(referencia_pid <= 400){
-    myPID.SetTunings(6.2,0.1,4.1);
-  }
-  else if(referencia_pid <=500){
-    myPID.SetTunings(6.2,0.1,6.1);
-  }
-  else if(referencia_pid<=600){
-    myPID.SetTunings(6.2,0.1,7.2);
-  }
+  myPID.SetTunings(Kp_prueba,0.1,(1.5/100)*referencia_pid+0.9);
   
    
 
@@ -105,41 +95,22 @@ void loop() {
   int timer_actual = millis();
   int tiempo = timer_actual - timer_start;
 
-
   
   float valor_actual = entrada_pid;
 
-  if(abs(valor_actual-valor_pasado) < 20){
+  if(abs(valor_actual-valor_pasado) < 20 && !volumen_alcanzado){
+
+
+    if(abs(entrada_pid - referencia_pid) < 0.5 || (entrada_pid - referencia_pid) > 0){
+      volumen_alcanzado = true;
+    }
+
     if(true){
       myPID.Compute();
       motoBomba.set_speed(salida_pid);
     }
-    else{
-
-      if(first_time){
-        time_mark  = millis();
-        first_time = false;
-      }
-      
-
-      if(entrada_pid < referencia_pid){
-        int time_since_mark = (timer_actual - time_mark);
-        if (time_since_mark < 1000){
-            motoBomba.set_speed(255);
-        }
-        else{
-            time_mark = millis();
-            motoBomba.set_speed(190);
-        }
-
-
-        }
-      else{
-        motoBomba.set_speed(85);
-      }
-    }}
   else{
-    motoBomba.set_speed(85);
+    motoBomba.set_speed(0);
   }
   
 
@@ -152,12 +123,20 @@ void loop() {
   if(!toggle){
     toggle = true;
   }
+}
+else{
+    motoBomba.set_speed(0);
 
+    
+}
   Serial.print(entrada_pid);
   Serial.print(",");
   Serial.println(tiempo);
   delay(20);
+
+
 }
+
 
 
 
