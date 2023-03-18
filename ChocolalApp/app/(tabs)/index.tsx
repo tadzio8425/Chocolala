50cc3042dfaf267fa9f47fd69b036c86c7aac040
tadzio8425 <js.ortegar1@uniandes.edu.co>
2023-03-17 22:45:28 -0500

    Index page

--- ChocolalApp/app/(tabs)/index.tsx
+++ ChocolalApp/app/(tabs)/index.tsx
@@ -1,14 +1,63 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, View, Image, Pressable, Alert} from 'react-native';
+import { useCallback, useState } from 'react';
 import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Text} from '../../components/Themed';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+
+SplashScreen.preventAutoHideAsync();
 
 export default function TabOneScreen() {
+
+  const [selected, setSelected] = useState(false);
+
+  //Cargar los Fonts
+  const [fontsLoaded] = useFonts({
+    'Source-Sans-Black': require('../../assets/fonts/SourceSans/SourceSansPro-Black.ttf'),
+    'Source-Sans-SemiBold': require('../../assets/fonts/SourceSans/SourceSansPro-SemiBold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
   return (
+    
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={{width:121, height:121}}>
+        
+      <Image
+        style={styles.image}
+        source={require("../../assets/images/hot-chocolate.png")}
+      />
+      </View>
+
+      <Text style={styles.title}>Chocolala</Text>
+
+      <Pressable 
+      onPressIn = {() => {
+        setSelected(!selected);
+     }}
+
+     onPressOut = {() => {
+      setSelected(!selected);
+   }}
+     
+     style={selected ? styles.unpressedButton: styles.pressedButton}
+     
+     >
+        <Text style={styles.buttonText}>Conectar</Text>
+
+      </Pressable>
+
     </View>
   );
 }
@@ -18,14 +67,68 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#FFFAF1', //Color de fondo del Tab
+    fontFamily: 'Source-Sans-Black'
   },
   title: {
-    fontSize: 20,
+    fontSize: 69,
     fontWeight: 'bold',
+    color:'#D49073',
+    textShadowColor: '#95573A',
+    textShadowOffset: {width: 2, height:2},
+    textShadowRadius: 2
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+
+  image: {
+    flex: 1,
+    width: '100%',
+  
+  },
+
+  pressedButton:{
+    backgroundColor:'#95573A',
+    width:145,
+    height:56,
+    borderRadius:60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+    marginBottom:100,
+    opacity: 0.7,
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+  },
+
+
+  unpressedButton:{
+    backgroundColor:'#A53D0D',
+    width:145,
+    height:56,
+    borderRadius:60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+    marginBottom:100,
+    opacity: 0.7,
+    shadowColor: '#000000',
+  },
+
+  buttonText:{
+    fontFamily: 'Source-Sans-SemiBold',
+    fontSize:20,
+    color:"#FFFAF1"
+  }
 });
+
+let myAdd = function (x: number, y: number): void {
+
+};
+
+
