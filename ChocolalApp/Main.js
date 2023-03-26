7010f181c95f450b421120a9f480bcaaa6252079
tadzio8425 <js.ortegar1@uniandes.edu.co>
2023-03-26 18:23:23 -0500

    tempy y peso

--- ChocolalApp/Main.js
+++ ChocolalApp/Main.js
@@ -1,49 +1,82 @@
 import { StyleSheet, View, Image, Pressable, Alert, Text} from 'react-native';
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 import AppLoading from "expo-app-loading";
 import useFonts from './hooks/useFonts';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const ESP32IP = 'http://192.168.1.4:8181/';
+const ESP32IP = 'http://192.168.1.4:8181';
 
 
-export default function Main({navigation}){
-    const [selected, setSelected] = useState(false);
-    return(
-        
+export default function Main({navigation}) {
+  const [dataJSON, setData] = useState(true);
+  const [weightJSON, setWeight] = useState(true);
+
+  const loadData = useCallback(async () => {
+    try {
+      const response = await fetch(`${ESP32IP}/`);
+      const dataJSON = await response.json();
+      setData(dataJSON);
+      setWeight(dataJSON[3]);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(loadData, 1000);
+    return () => clearInterval(interval);
+  }, [loadData]);
+
+  return (
     <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Image
+          style={{width: 60, height: 60}}
+          source={require("./assets/images/hot-chocolate.png")}
+        />
+        <Text style={styles.title}>Chocolala</Text>
+      </View>
 
-  </View>
-    );
+      <View style={styles.innerConntainer}>
+        <Text style={styles.variableText}>Peso</Text>
+        <TextBox text={weightJSON["value"]} units={weightJSON["unit"]}></TextBox>
+        <Text style={[styles.variableText, {marginLeft: "5%"}]}>Temperatura</Text>
+        <TextBox text="yyy" units="°C"></TextBox>
+      </View>
+    </View>
+  );
 }
+
+
+const TextBox = ({text, units}) => (
+  <View style = {styles.textBox}>
+    <Text style={styles.numberText}>{text} {units}</Text>
+  </View>
+)
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
+      alignItems:"flex-start",
+      justifyContent: "flex-start",
       backgroundColor: '#FFFAF1', //Color de fondo del Tab
       fontFamily: 'Source-Sans-Black'
     },
     title: {
-      fontSize: 69,
+      fontSize: 36,
       fontWeight: 'bold',
       color:'#D49073',
       textShadowColor: '#95573A',
-      textShadowOffset: {width: 2, height:2},
-      textShadowRadius: 2
+      textShadowOffset: {width: 1, height:1},
+      textShadowRadius: 2,
+      paddingTop:20,
+      paddingLeft:"3%"
     },
     separator: {
       marginVertical: 30,
       height: 1,
       width: '80%',
-    },
-  
-    image: {
-      flex: 1,
-      width: '100%',
-    
     },
   
     pressedButton:{
@@ -60,6 +93,18 @@ const styles = StyleSheet.create({
       shadowOffset: {width: -2, height: 4},
       shadowOpacity: 0.4,
       shadowRadius: 3,
+    },
+
+    textBox:{
+      backgroundColor:'#95573A',
+      width:91,
+      height:35,
+      borderRadius:30,
+      alignItems: 'center',
+      justifyContent: 'center',
+      opacity: 0.54,
+      marginLeft:"2%",
+      textAlign:"center"
     },
   
   
@@ -80,8 +125,43 @@ const styles = StyleSheet.create({
       fontFamily: 'Source-Sans-SemiBold',
       fontSize:20,
       color:"#FFFAF1"
+    },
+    titleContainer:{
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      alignItems: 'flex-start', // if you want to fill rows left to right
+      width:"100%",
+      maxHeight:60,
+      marginTop:"10%",
+      marginLeft:"5%",
+      alignSelf:"start"
+    },
+
+    innerConntainer:{
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      width:"100%",
+      maxHeight:60,
+      marginTop:"10%",
+      marginLeft:"7%",
+      alignItems:"center"
+    },
+
+    variableText:{
+      fontFamily: 'Source-Sans-SemiBold',
+      fontSize:20,
+      color:"#535353"
+    },
+
+    numberText:{
+      fontFamily: 'Source-Sans-Regular',
+      fontSize:20
     }
-  });
+
+    }
+  );
   
 
   
--- ChocolalApp/hooks/useFonts.js
+++ ChocolalApp/hooks/useFonts.js
@@ -5,4 +5,5 @@ export default useFonts  = async () =>
     await Font.loadAsync({
         'Source-Sans-Black': require('../assets/fonts/SourceSans/SourceSansPro-Black.ttf'),
         'Source-Sans-SemiBold': require('../assets/fonts/SourceSans/SourceSansPro-SemiBold.ttf'),
+        'Source-Sans-Regular': require('../assets/fonts/SourceSans/SourceSansPro-Regular.ttf')
   });
