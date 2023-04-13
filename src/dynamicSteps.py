f57f255472e981efd69795e19a6023267e3599f0
t4zdi0Neo <js.ortegar1@uniandes.edu.co>
2023-04-13 14:29:19 -0500

    crack

--- src/dynamicSteps.py
+++ src/dynamicSteps.py
@@ -1,27 +1,43 @@
 microStep_list = [1/16, 1/8, 1/4, 1/2, 1]
-limitSteps = 5
+desiredStep = 5.625
 
-def getSteps(usableSteps: int, stepValue: int):
+microStep_list = [1/16, 1/8, 1/4, 1/2, 1]
 
-    #Caso base: No se considera ningún step pero tampoco se necesita ningun valor
-    if(usableSteps == 0 and stepValue == 0):
-        return True
-    
-    #Caso base: No se considera ningún step pero se necesita un valor
-    if(usableSteps == 0 and stepValue != 0):
-        return False
-    
-    #Caso base: No se necesita ningún step
-    if(stepValue == 0):
-        return True
-    
-    #Caso recursivo: El step considerado es muy grande
-    if(microStep_list[usableSteps-1] > stepValue  and    (usableSteps != 0 and stepValue != 0)):
-        return getSteps(usableSteps - 1, stepValue)
-    
-    #Caso recursivo: El step considerado si se puede tomar en cuenta
-    if(microStep_list[usableSteps-1] <= stepValue  and    (usableSteps != 0 and stepValue != 0)):
-        return getSteps(usableSteps - 1, stepValue) or getSteps(usableSteps - 1, stepValue - microStep_list[usableSteps-1]) or getSteps(usableSteps, stepValue - microStep_list[usableSteps])
+microStep_list = [1/16, 1/8, 1/4, 1/2, 1]
+
+def getSteps(usableSteps: int, stepValue: float) -> bool:
+    memo = {}
+
+    def memoizedGetSteps(usableSteps: int, stepValue: float) -> list:
+        if (usableSteps, stepValue) in memo:
+            return memo[(usableSteps, stepValue)]
+
+        if usableSteps == 0 and stepValue == 0:
+            return []
+
+        if usableSteps == 0 and stepValue != 0:
+            return None
+
+        if stepValue == 0:
+            return []
+
+        if microStep_list[usableSteps-1] > stepValue:
+            result = memoizedGetSteps(usableSteps - 1, stepValue)
+        else:
+            result1 = memoizedGetSteps(usableSteps - 1, stepValue)
+            result2 = memoizedGetSteps(usableSteps - 1, stepValue - microStep_list[usableSteps-1])
+            result3 = memoizedGetSteps(usableSteps, stepValue - microStep_list[usableSteps-1])
+            options = [result for result in [result1, result2, result3] if result is not None]
+            if not options:
+                result = None
+            else:
+                result = [microStep_list[usableSteps-1]] + min(options, key=len)
+
+        memo[(usableSteps, stepValue)] = result
+        return result
+
+    return memoizedGetSteps(len(microStep_list), stepValue)
 
 
-print(getSteps(len(microStep_list), 3))
+steps = getSteps(len(microStep_list), desiredStep)
+print(steps)
