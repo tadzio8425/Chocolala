15ba24d0a7c9dab5a4ceaa65d9eec1506637ee37
t4zdi0Neo <js.ortegar1@uniandes.edu.co>
2023-04-13 02:51:39 -0500

    Sangre, sudor y lagrimas

--- src/main.cpp
+++ src/main.cpp
@@ -26,12 +26,14 @@ int pump_PWM = 14;
 #define ENCODER 34
 
 //HARDWIRED A GND - NO CONECTAR!
-#define MS1 0
-#define MS2 0
-#define MS3 0
+#define MS1 15
+#define MS2 2
+#define MS3 4
+
+double FULL_STEP = 1;
 
 //VARIABLES CONTROL MOTOR
-int encoder_counter = 0;
+double encoder_counter = 0;
 
 //Variable - calibración
 int pendiente = 627.643083;
@@ -73,15 +75,80 @@ FirebaseT iotHandler;
 TaskHandle_t motorTask;
 
 bool pulseToggle = false;
+//Pulsos del motor
+double errorTol = 0.1;
+
+void setMicrostep(int ms1Val, int ms2Val, int ms3Val){
+  digitalWrite(MS1, ms1Val);
+  digitalWrite(MS2, ms2Val);
+  digitalWrite(MS3, ms3Val);
+}
 
 void motorPulse(int del){
-
     digitalWrite(STEP, HIGH);
     delay(del);
     digitalWrite(STEP, LOW);
-    delay(del);
-
   }
+
+void motorSmartPulse(double mean, double numPulsos, double numerator, double delayDeseado, double invDelayCalculado){
+
+    if(0.01 < abs(invDelayCalculado - 1 / delayDeseado) ){
+
+      if(FULL_STEP - mean <= mean){
+        setMicrostep(LOW, LOW, LOW);
+        Serial.println("FULL");
+        numerator += FULL_STEP;
+        mean = FULL_STEP - mean;
+      }
+      else if((FULL_STEP/(double)2) - mean <= mean){
+        setMicrostep(HIGH, LOW, LOW);
+        Serial.println("HALF");
+        numerator += FULL_STEP/(double)2;
+        mean = (FULL_STEP/(double)2) - mean/(double)2;
+      }
+
+      else if((FULL_STEP/(double)4) - mean <= mean){
+        setMicrostep(LOW, HIGH, LOW);
+        Serial.println("QUARTER");
+        numerator += FULL_STEP/(double)4;
+        mean = (FULL_STEP/(double)4) - mean/(double)4;
+      }
+
+      else if((FULL_STEP/(double)8) - mean <= mean){
+        setMicrostep(HIGH, HIGH, LOW);
+        Serial.println("EIGHT");
+        numerator += FULL_STEP/(double)8;
+        mean = (FULL_STEP/(double)8) - mean/(double)8;
+      }
+
+      else if((FULL_STEP/(double)16) - mean <= mean){
+        setMicrostep(HIGH, HIGH, HIGH);
+        Serial.println("SIXTEENTH");
+        numerator += FULL_STEP/(double)16;
+        mean = (FULL_STEP/(double)16) - mean/(double)16;
+      }
+
+      invDelayCalculado = numerator/numPulsos;
+      
+      //Serial.println(numPulsos);
+
+      digitalWrite(STEP, HIGH);
+      delay(1);
+      digitalWrite(STEP, LOW);
+
+      motorSmartPulse(mean, numPulsos + 1, numerator, delayDeseado, invDelayCalculado);
+    }
+    else{
+      Serial.println("Fuera");
+    }
+    
+  }
+
+
+
+
+
+
 
 
 
@@ -114,8 +181,9 @@ void motorTaskCode( void * pvParameters ){
   Serial.println(xPortGetCoreID());
 
   for(;;){
-    motorPulse(1);
 
+  
+    motorSmartPulse(1/1.9, 1, 0, 1.9, 1000);
     int valorEncoder = digitalRead(ENCODER);
 
     double stepsInTime = motorCount(valorEncoder, 5000);
@@ -140,8 +208,17 @@ void setup() {
 
   pinMode(DIR, OUTPUT);
   pinMode(STEP, OUTPUT);
+
+  pinMode(MS1, OUTPUT);
+  pinMode(MS2, OUTPUT);
+  pinMode(MS3, OUTPUT);
+
+  digitalWrite(MS1, HIGH);
+  digitalWrite(MS2, HIGH);
+  digitalWrite(MS3, HIGH);
+  
   pinMode(ENCODER, INPUT);
-  digitalWrite(DIR,LOW);
+  digitalWrite(DIR,HIGH);
 
   //Baudiaje de la comunicación serial
   Serial.begin(115200);
@@ -152,7 +229,7 @@ void setup() {
 
   //Modo wireless (WiFi + Firebase)
   if(wireless_mode){
-    iotHandler.setWiFi("Chocolala", "chocolele", true);
+    iotHandler.setWiFi("Chocolala", "chocolele", false);
     //iotHandler.setFirebase("AIzaSyAiaVAvazH57Fce9ZsE9Cm06BxBMsoJXXw", "https://chocolala-e8384-default-rtdb.firebaseio.com/");
     iotHandler.setWebServer(80);
     ChocolalaREST::linkServer(iotHandler.getServerPointer());
